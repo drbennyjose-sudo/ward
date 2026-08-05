a8c745da5fe428220dd9c6703092ac220c04fe03
Benny Jose Panakkal <drbennyjose@gmail.com>
2026-08-05 22:07:17 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+window.WARD_CONFIG = {
+  url: "https://qxcdadzpjeyhufqnilja.supabase.co",
+  key: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF4Y2RhZHpwamV5aHVmcW5pbGphIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU5Mzg5MDYsImV4cCI6MjEwMTUxNDkwNn0.yLHg786Z5rQf_5QPzEAir-LMhHFn8z0N-_VUvpGK4w4"
+};
